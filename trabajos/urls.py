d82769d4043105cbf2f153d0065714e3cea2843c
Tu Nombre <karinamelisa26@gmail.com>
2025-10-26 20:27:38 -0300

    Actualizo logo y templates de trabajos

--- trabajos/urls.py
+++ trabajos/urls.py
@@ -5,8 +5,8 @@ app_name = 'trabajos'
 
 urlpatterns = [
     path('', views.lista_trabajos, name='lista_trabajos'),
-   path('agregar/', views.agregar_trabajo, name='agregar_trabajo'),
-    path('editar/<int:trabajo_id>/', views.editar_trabajo, name='editar'),
-    path('ocultar/<int:trabajo_id>/', views.ocultar_trabajo, name='ocultar'),
-    path('<int:id>/', views.detalle_trabajo, name='detalle'),
+   path('agregar/', views.agregar_trabajo, name='agregar_trabajos'),
+    path('editar/<int:trabajo_id>/', views.editar_trabajo, name='editar_trabajo'),
+    path('ocultar/<int:trabajo_id>/', views.ocultar_trabajo, name='ocultar_trabajo'),
+    path('<int:id>/', views.detalle_trabajo, name='detalle_trabajo'),
 ]
